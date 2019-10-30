09fdd07ac3385f5ec41e0d1ae5d87429b5add087
smeyer <smeyer.ilias@gmx.de>
2019-10-30 17:27:03 +0100

    Increased plugin version

--- plugin.php
+++ plugin.php
@@ -2,11 +2,11 @@
 // alphanumerical ID of the plugin; never change this
 $id = "otxt";
 // code version; must be changed for all code changes
-$version = '5.3.14.1';
+$version = '5.4.14.2';
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.3.0";
-$ilias_max_version = "5.3.999";
+$ilias_min_version = "5.4.0";
+$ilias_max_version = "5.4.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Stefan Meyer";
